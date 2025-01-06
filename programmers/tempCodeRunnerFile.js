635aa58ccc25ecb3f7fd9c175d4cad5a0c63d6f3
MyungJiwoo <1206jiwoo@gmail.com>
2025-01-06 10:14:49 +0900

    Update Readme

---
+++ programmers/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let input = [];
+
+// rl.on("line", function (line) {
+//     input = line.split(" ");
+// }).on("close", function () {
+//     console.log(Number(input[0]) + Number(input[1]));
+// });
