605789188c1c4aa6dc728361f5408aa7d137f435
MyungJiwoo <1206jiwoo@gmail.com>
2025-08-29 13:50:30 +0900

    2025.08.29

---
+++ programmers/Level_0/P181877.js
@@ -0,0 +1,11 @@
+// P181877: 대소문자로 바꾸기
+
+function solution(myString) {
+  return myString.toUpperCase();
+}
+
+console.log(solution("aBcDeFg"));
+
+/**
+ * String을 모두 대/소문자로 바꾸려면 toUpperCase/toLowerCase 사용
+ */
