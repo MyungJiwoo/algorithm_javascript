e02a3ef99f2ad3b276448e1efd126ee18a77c542
MyungJiwoo <1206jiwoo@gmail.com>
2025-09-26 11:40:03 +0900

    2025.09.26

---
+++ programmers/Level_0/P120816.js
@@ -0,0 +1,13 @@
+// P120816: 피자 나눠먹기 (3)
+
+const solution = (slice, n) => {
+  return Math.ceil(n / slice);
+};
+
+console.log(solution(7, 10));
+console.log(solution(4, 12));
+
+/**
+ * 문제 자체는 어렵지 않았다.
+ * 그저 오랜만에 풀었더니 꾸준히 해야겠다는 생각만...
+ */
