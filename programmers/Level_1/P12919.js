855b1d33efbdafb464a7bed76531b241fa676ffd
MyungJiwoo <1206jiwoo@gmail.com>
2025-11-06 10:44:57 +0900

    2025.11.06

---
+++ programmers/Level_1/P12919.js
@@ -0,0 +1,16 @@
+// P12919: 서울에서 김서방 찾기
+function solution(seoul) {
+  return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
+}
+
+console.log(solution(["Jane", "Kim"]));
+
+/**
+ * 주어진 String 배열에서 "Kim"의 위치를 반환한다.
+ * - 주어진 배열에서 "Kim"의 위치를 찾아 반환한다.
+ */
+
+/**
+ * 너무 금방 풀었다...
+ * 풀이 비교해도 크게 차이나는건 없었다.
+ */
